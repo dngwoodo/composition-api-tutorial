e1713b0ad00e775e2cef577bdcefcaf906045a9f
dngwoodo <dngwoodo@gmail.com>
2021-03-22 10:15:52 +0900

    env: Setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,25 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      2, {
+        singleQuote: true,
+        semi: true,
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+      }
+    ]
   },
   overrides: [
     {
